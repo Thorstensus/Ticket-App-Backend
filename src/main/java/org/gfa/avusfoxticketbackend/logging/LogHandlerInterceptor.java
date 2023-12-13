d345fb898e9259053cab9111d65e8a5a6c5531b8
Štěpán Kostolány <stepan.kostolany@seznam.cz>
2023-12-13 17:20:13 +0100

    fix(): parameters fixed for query, request body still null

--- src/main/java/org/gfa/avusfoxticketbackend/logging/LogHandlerInterceptor.java
+++ src/main/java/org/gfa/avusfoxticketbackend/logging/LogHandlerInterceptor.java
@@ -8,28 +8,30 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
+
 @Component
 public class LogHandlerInterceptor implements HandlerInterceptor {
 
-    Logger logger = LoggerFactory.getLogger(LogHandlerInterceptor.class);
+  Logger logger = LoggerFactory.getLogger(LogHandlerInterceptor.class);
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info("Type:{} Endpoint:{} Parameters:{} Status:{}", request.getMethod(), request.getRequestURI(), request.getParameterMap(), response.getStatus());
-        return true;
-    }
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    logger.info("Type:{} Endpoint:{} Parameters:{} Status:{}", request.getMethod(), request.getRequestURI(), request.getQueryString(), response.getStatus());
+    return true;
+  }
 
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        logger.info("Type:{} Endpoint:{} Parameters:{} Status:{}", request.getMethod(), request.getRequestURI(), request.getParameterMap(), response.getStatus());
-    }
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    logger.info("Type:{} Endpoint:{} Parameters:{} Status:{}", request.getMethod(), request.getRequestURI(), request.getQueryString(), response.getStatus());
+  }
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        if(response.getStatus() >= 400) {
-            logger.error("Type:{} Endpoint:{} Parameters:{} Status:{}", request.getMethod(), request.getRequestURI(), request.getParameterMap(), response.getStatus());
-        } else {
-            logger.info("Type:{} Endpoint:{} Parameters:{} Status:{}", request.getMethod(), request.getRequestURI(), request.getParameterMap(), response.getStatus());
-        }
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    if(response.getStatus() >= 400) {
+      logger.error("Type:{} Endpoint:{} Parameters:{} Status:{}", request.getMethod(), request.getRequestURI(), request.getQueryString(), response.getStatus());
+    } else {
+      logger.info("Type:{} Endpoint:{} Parameters:{} Status:{}", request.getMethod(), request.getRequestURI(), request.getQueryString(), response.getStatus());
     }
+  }
 }
