7d680a9ae97ddcfbefd273cc798b63c80e90baac
belaerdos <erdosbelamusic@gmail.com>
2023-12-13 15:40:21 +0100

    feat: create JwtService with methods

--- src/main/java/org/gfa/avusfoxticketbackend/config/JwtAuthenticationFilter.java
+++ src/main/java/org/gfa/avusfoxticketbackend/config/JwtAuthenticationFilter.java
@@ -4,18 +4,37 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    @Autowired
+    public JwtAuthenticationFilter(JwtService jwtService){
+        this.jwtService = jwtService;
+    }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain)
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain)
                             throws ServletException, IOException {
-        
+        final String authHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        final String userEmail;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwtToken);
 
     }
 }
