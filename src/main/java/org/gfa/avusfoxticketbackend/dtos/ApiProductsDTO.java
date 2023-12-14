e34bbb074357ff91cedac95aa78bc7b7ffd98533
chabyie <137812166+chabyie@users.noreply.github.com>
2023-12-14 13:25:49 +0100

    Setup continuous integration for the project (#10)
    * Initial commit
    
    * Create BuildStyleCheck.yml
    
    * Fixed .gitignore
    
    * Delete .env
    
    * Update and rename BuildStyleCheck.yml to build-test-stylecheck.yml
    
    * Added checkstyle config
    
    * Fixed the code so it can pass the stylecheck
    
    * Delete AvusFoxticketBackendApplicationTests.java

--- src/main/java/org/gfa/avusfoxticketbackend/dtos/ApiProductsDTO.java
+++ src/main/java/org/gfa/avusfoxticketbackend/dtos/ApiProductsDTO.java
@@ -6,21 +6,21 @@ import org.gfa.avusfoxticketbackend.dtos.abstractdtos.ResponseDTO;
 
 public class ApiProductsDTO extends ResponseDTO {
 
-    private List<ProductDTO> products;
+  private List<ProductDTO> products;
 
-    public ApiProductsDTO() {
-        this.products = new ArrayList<>();
-    }
+  public ApiProductsDTO() {
+    this.products = new ArrayList<>();
+  }
 
-    public ApiProductsDTO(List<ProductDTO> products) {
-        this.products = products;
-    }
+  public ApiProductsDTO(List<ProductDTO> products) {
+    this.products = products;
+  }
 
-    public List<ProductDTO> getProducts() {
-        return products;
-    }
+  public List<ProductDTO> getProducts() {
+    return products;
+  }
 
-    public void setProducts(List<ProductDTO> products) {
-        this.products = products;
-    }
+  public void setProducts(List<ProductDTO> products) {
+    this.products = products;
+  }
 }
--- src/main/java/org/gfa/avusfoxticketbackend/exeption/ErrorResponse.java
+++ src/main/java/org/gfa/avusfoxticketbackend/exeption/ErrorResponse.java
@@ -1,35 +1,36 @@
 package org.gfa.avusfoxticketbackend.exeption;
 
 import java.time.ZonedDateTime;
-import org.gfa.avusfoxticketbackend.timeFormatter.TimeFormatter;
+import org.gfa.avusfoxticketbackend.timeformater.TimeFormatter;
 import org.springframework.http.HttpStatus;
 
 public class ErrorResponse {
-    private final HttpStatus httpStatus;
-    private final String endpoint;
-    private final String message;
-    private final String time;
+  private final HttpStatus httpStatus;
+  private final String endpoint;
+  private final String message;
+  private final String time;
 
-    public ErrorResponse(HttpStatus httpStatus, String endpoint, String message, ZonedDateTime timestamp) {
-        this.httpStatus = httpStatus;
-        this.endpoint = endpoint;
-        this.message = message;
-        this.time = TimeFormatter.getFormattedTimestamp(timestamp);
-    }
+  public ErrorResponse(
+      HttpStatus httpStatus, String endpoint, String message, ZonedDateTime timestamp) {
+    this.httpStatus = httpStatus;
+    this.endpoint = endpoint;
+    this.message = message;
+    this.time = TimeFormatter.getFormattedTimestamp(timestamp);
+  }
 
-    public String getMessage() {
-        return message;
-    }
+  public String getMessage() {
+    return message;
+  }
 
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
+  public HttpStatus getHttpStatus() {
+    return httpStatus;
+  }
 
-    public String getTime() {
-        return time;
-    }
+  public String getTime() {
+    return time;
+  }
 
-    public String getEndpoint() {
-        return endpoint;
-    }
+  public String getEndpoint() {
+    return endpoint;
+  }
 }
