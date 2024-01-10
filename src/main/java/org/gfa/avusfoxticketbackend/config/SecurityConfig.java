2cb0fc50dbcf9bb06b33aa020a9038a98e453c2c
Petr Půta <petr.puta1@gmail.com>
2024-01-10 15:50:01 +0100

    feat: added method authentication for ADMIN role for admin endpoints

--- src/main/java/org/gfa/avusfoxticketbackend/config/SecurityConfig.java
+++ src/main/java/org/gfa/avusfoxticketbackend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
   private final JwtAuthenticationFilter jwtAuthFilter;
   private final AuthenticationProvider authenticationProvider;
@@ -35,8 +37,10 @@ public class SecurityConfig {
         .authorizeHttpRequests(
             authorizeHttpRequests ->
                 authorizeHttpRequests
-                    .requestMatchers("/api/news", "/api/users", "/api/users/login")
+                    .requestMatchers("/api/news/**", "/api/users", "/api/users/login")
                     .permitAll()
+                    .requestMatchers("/api/admin//**")
+                    .hasRole("ADMIN")
                     .anyRequest()
                     .authenticated())
         .sessionManagement(
