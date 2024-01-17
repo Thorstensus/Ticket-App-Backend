6239951426da44a2240eb825c32e0768efbce538
chabyie <137812166+chabyie@users.noreply.github.com>
2024-01-17 17:41:46 +0100

    AFB-37-email-service (#21)
    * Add email service
    
    * Add sensitive data to .env file
    
    * fix(): fix bug where multiple users could register under the same email
    
    * Delete test enpoint from unsecured endpoints in SecurityConfig
    
    * Extracted interface from JwtService, write tests for OrderServiceImpl and /api/order endpoint
    
    * Fix(): add mail port and verification link to .env
    
    * Fix(): changed UserServiceImplTest to use mockbean to work on github actions
    
    * checkstyle
    
    * delete enviroment variables to pass test on UserServiceImpl
    
    * damn you checkstyle
    
    * Put email template to 3 separate txt files to ensure custom name and link compatibility, add method that gets the template from the txt file. Also added tests to EmailService
    
    * adding anotation to fix failed tests
    
    * Destroyed EmailServiceTest
    
    * Add @Service annotation to EmailSender, reintroduced EmailService tests
    
    * Destroyed email service tests once again
    
    * Implemented exception to getEmailHtmlTemplate method, removed @Service annotation from interface
    
    * Remove unused import

---
+++ src/main/java/org/gfa/avusfoxticketbackend/email/EmailService.java
@@ -0,0 +1,73 @@
+package org.gfa.avusfoxticketbackend.email;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import org.gfa.avusfoxticketbackend.services.ExceptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService implements EmailSender {
+
+  private static final Dotenv dotenv = Dotenv.configure().load();
+  private static final String MAIL_USERNAME = dotenv.get("MAIL_USERNAME");
+  private final JavaMailSender mailSender;
+
+  private final ExceptionService exceptionService;
+
+  @Autowired
+  public EmailService(JavaMailSender mailSender, ExceptionService exceptionService) {
+    this.mailSender = mailSender;
+    this.exceptionService = exceptionService;
+  }
+
+  @Override
+  public String getEmailHtmlTemplate(String templatePath) {
+    Path path = Path.of(templatePath);
+    String output = "";
+
+    try {
+      List<String> templateLines = Files.readAllLines(path);
+      for (String line : templateLines) {
+        output += line;
+      }
+    } catch (IOException e) {
+      exceptionService.throwFailedToGetEmailTemplate();
+    }
+    return output;
+  }
+
+  @Override
+  @Async
+  public void send(String to, String email) {
+    try {
+      MimeMessage mimeMessage = mailSender.createMimeMessage();
+      MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+      helper.setText(email, true);
+      helper.setTo(to);
+      helper.setSubject("Confirm your email");
+      helper.setFrom(MAIL_USERNAME);
+      mailSender.send(mimeMessage);
+    } catch (MessagingException e) {
+      System.out.println("failed to send email");
+      throw new IllegalStateException("failed to send email");
+    }
+  }
+
+  public String buildEmail(String name, String link) {
+    return getEmailHtmlTemplate("src/main/resources/templates/email-template.txt")
+        + " "
+        + name
+        + getEmailHtmlTemplate("src/main/resources/templates/email-template2.txt")
+        + link
+        + getEmailHtmlTemplate("src/main/resources/templates/email-template3.txt");
+  }
+}
