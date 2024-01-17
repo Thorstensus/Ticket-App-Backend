9be0a8501aeceda67286fe953599a1b8214e5ebc
chabyie <dominik.cajan1@gmail.com>
2024-01-17 14:07:50 +0100

    adding anotation to fix failed tests

--- src/test/java/org/gfa/avusfoxticketbackend/email/EmailServiceTest.java
+++ src/test/java/org/gfa/avusfoxticketbackend/email/EmailServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -19,6 +20,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ContextConfiguration(classes = {EmailService.class})
 @ExtendWith(SpringExtension.class)
+@SpringBootTest
 class EmailServiceTest {
   @Autowired private EmailService emailService;
 
