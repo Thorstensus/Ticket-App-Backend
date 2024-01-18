a05c87c68da88259f369e31746abefe9038eae86
PPetr1 <139972286+PPetr1@users.noreply.github.com>
2024-01-18 16:03:08 +0100

    Afb 39 my purchases (#24)
    * feat(): implemented getOrders endpoint
    
    * refractor(): moved /orders endpoints to each other
    
    * fix(): remade method for printing current order directly from cart and not the whole order list in post/orders endpoint
    
    * style check
    
    * added endpoint test for post /cart
    
    * fix(): fixed cartSendOk test
    
    * added test
    
    * optimized imports
    
    * testOrder breaks
    
    * Delete SecuredControllerTest.java
    
    * checkstyle

--- src/main/java/org/gfa/avusfoxticketbackend/controllers/SecuredController.java
+++ src/main/java/org/gfa/avusfoxticketbackend/controllers/SecuredController.java
@@ -38,16 +38,6 @@ public class SecuredController {
     this.orderService = orderService;
   }
 
-  @PostMapping("/orders")
-  public ResponseEntity<ResponseOrderSummaryDTO> order(
-      @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
-    LogHandlerInterceptor.object = token;
-    token = token.substring(7);
-    userService.checkUserVerification(token);
-    orderService.saveOrdersFromCart(token);
-    return ResponseEntity.status(200).body(orderService.getOrderSummaryDTO(token));
-  }
-
   @PostMapping("/cart")
   public ResponseEntity<CartResponseDTO> addToCart(
       @RequestBody(required = false) CartRequestDTO cartRequestDTO,
@@ -55,5 +45,23 @@ public class SecuredController {
     LogHandlerInterceptor.object = List.of(cartRequestDTO, httpServletRequest);
     return ResponseEntity.status(200)
         .body(userService.saveProductToCart(cartRequestDTO, httpServletRequest));
+  }
+
+  @PostMapping("/orders")
+  public ResponseEntity<ResponseOrderSummaryDTO> order(
+      @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+    LogHandlerInterceptor.object = token;
+    token = token.substring(7);
+    userService.checkUserVerification(token);
+    orderService.saveOrdersFromCart(token);
+    return ResponseEntity.status(200).body(orderService.getCartOrderSummaryDTOandCleanCart(token));
+  }
+
+  @GetMapping("/orders")
+  public ResponseEntity<ResponseOrderSummaryDTO> getAllOrders(
+      @RequestHeader(HttpHeaders.AUTHORIZATION) String token) {
+    LogHandlerInterceptor.object = token;
+    token = token.substring(7);
+    return ResponseEntity.status(200).body(orderService.getOrderSummaryDTO(token));
   }
 }
