f3c56981056547ecc154a7480a2cecd60ef0aa0a
Petr Půta <petr.puta1@gmail.com>
2024-02-01 14:39:36 +0100

    Added component scan annotation to main app

--- src/main/java/org/gfa/avusfoxticketbackend/AvusFoxticketBackendApplication.java
+++ src/main/java/org/gfa/avusfoxticketbackend/AvusFoxticketBackendApplication.java
@@ -3,10 +3,12 @@ package org.gfa.avusfoxticketbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@ComponentScan({ "org.gfa.avusfoxticketbackend.*" })
 public class AvusFoxticketBackendApplication {
 
   public static void main(String[] args) {
