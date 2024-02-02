612a0a0bc61d5ae972de1fdc9b6fc9037d37e793
Petr Půta <petr.puta1@gmail.com>
2024-02-02 10:14:41 +0100

    refractor(): unused import

--- src/main/java/org/gfa/avusfoxticketbackend/AvusFoxticketBackendApplication.java
+++ src/main/java/org/gfa/avusfoxticketbackend/AvusFoxticketBackendApplication.java
@@ -2,7 +2,6 @@ package org.gfa.avusfoxticketbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
